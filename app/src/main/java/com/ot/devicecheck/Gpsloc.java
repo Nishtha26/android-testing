dccceaeea7c3a3caf8bf8c22069fc5f368dcd09b
Pranjal Gupta <pgupta1996@gmail.com>
2018-01-20 21:42:32 +0530

    UI enhanced for Accelerometer, Battery Indicator, Gps, Gravity, Magnetic and Headphone

--- app/src/main/java/com/ot/devicecheck/Gpsloc.java
+++ app/src/main/java/com/ot/devicecheck/Gpsloc.java
@@ -1,33 +1,76 @@
 package com.ot.devicecheck;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 
-public class Gpsloc extends Activity{
-    
-   TextView tr,ts;
-   double latitude,longitude;
-    
+public class Gpsloc extends Activity {
+
+    TextView tr, ts;
+    double latitude, longitude;
+    SQLElement element;
+    DatabaseHandler db;
+    DialogInterface.OnClickListener dialogClickListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gpsloc);
         Gpslocate gps = new Gpslocate(this);
         latitude = gps.getLatitude();
         longitude = gps.getLongitude();
-        tr=(TextView)findViewById(R.id.textView60);
-        ts=(TextView)findViewById(R.id.textView61);
-        
+        tr = (TextView) findViewById(R.id.textView60);
+        ts = (TextView) findViewById(R.id.textView61);
+
+        db = new DatabaseHandler(this);
+
+        dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                switch (i) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        Log.i("Positive", "Clicked");
+                        element = new SQLElement();
+                        element.setElement("GPS");
+                        element.setVerdict("Working");
+                        element.setTimeStamp("" + System.currentTimeMillis());
+                        db.addElement(element);
+                        db.updateElement(element);
+                        finish();
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        Log.i("Negative", "Clicked");
+                        element = new SQLElement();
+                        element.setElement("GPS");
+                        element.setVerdict("Not Working");
+                        element.setTimeStamp("" + System.currentTimeMillis());
+                        db.addElement(element);
+                        db.updateElement(element);
+                        finish();
+                        break;
+                }
+            }
+        };
+
     }
 
-	@Override
-	protected void onStart() {
-		tr.setText("latitude "+String.valueOf(latitude));
-		ts.setText("longitude "+String.valueOf(longitude));
-		super.onStart();
-	}
-    
+    @Override
+    public void onBackPressed() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Gpsloc.this);
+        builder.setMessage("Is it functionality working properly?").setPositiveButton("Yes", dialogClickListener).setNegativeButton("No", dialogClickListener).show();
     }
+
+    @Override
+    protected void onStart() {
+        tr.setText("latitude " + String.valueOf(latitude));
+        ts.setText("longitude " + String.valueOf(longitude));
+        super.onStart();
+    }
+
+}
--- app/src/main/java/com/ot/devicecheck/Headphone.java
+++ app/src/main/java/com/ot/devicecheck/Headphone.java
@@ -1,76 +1,121 @@
 package com.ot.devicecheck;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-public class Headphone extends Activity  {
-private MusicIntentReceiver myReceiver;
-TextView s;
 
-@Override 
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_headphone);
-    
-    s=(TextView)findViewById(R.id.textView14);
-    myReceiver = new MusicIntentReceiver();
-}
+public class Headphone extends Activity {
+    private MusicIntentReceiver myReceiver;
+    TextView s;
+    SQLElement element;
+    DatabaseHandler db;
+    DialogInterface.OnClickListener dialogClickListener;
 
-@Override 
-public void onResume() {
-    IntentFilter filter = new IntentFilter(Intent.ACTION_HEADSET_PLUG);
-    registerReceiver(myReceiver, filter);
-    super.onResume();
-}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_headphone);
 
-private class MusicIntentReceiver extends BroadcastReceiver {
-    @Override public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(Intent.ACTION_HEADSET_PLUG)) {
-            int state = intent.getIntExtra("state", -1);
-            switch (state) {
-            case 0:
-                s.setText("Headset is unplugged");
-                break;
-            case 1:
-                s.setText("Headset is plugged");
-                break;
-            default:
-                s.setText("Plugin the headset");
+        db = new DatabaseHandler(this);
+
+        dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                switch (i) {
+                    case DialogInterface.BUTTON_POSITIVE:
+                        Log.i("Positive", "Clicked");
+                        element = new SQLElement();
+                        element.setElement("Headphone");
+                        element.setVerdict("Working");
+                        element.setTimeStamp("" + System.currentTimeMillis());
+                        db.addElement(element);
+                        db.updateElement(element);
+                        finish();
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        Log.i("Negative", "Clicked");
+                        element = new SQLElement();
+                        element.setElement("Headphone");
+                        element.setVerdict("Not Working");
+                        element.setTimeStamp("" + System.currentTimeMillis());
+                        db.addElement(element);
+                        db.updateElement(element);
+                        finish();
+                        break;
+                }
+            }
+        };
+
+        s = (TextView) findViewById(R.id.textView14);
+        myReceiver = new MusicIntentReceiver();
+    }
+
+    @Override
+    public void onResume() {
+        IntentFilter filter = new IntentFilter(Intent.ACTION_HEADSET_PLUG);
+        registerReceiver(myReceiver, filter);
+        super.onResume();
+    }
+
+    private class MusicIntentReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_HEADSET_PLUG)) {
+                int state = intent.getIntExtra("state", -1);
+                switch (state) {
+                    case 0:
+                        s.setText("Headset is unplugged");
+                        break;
+                    case 1:
+                        s.setText("Headset is plugged");
+                        break;
+                    default:
+                        s.setText("Plugin the headset");
+                }
             }
         }
     }
-}
 
-@Override 
+    @Override
 
-public void onPause() {
-    unregisterReceiver(myReceiver);
-    super.onPause();
-}
+    public void onPause() {
+        unregisterReceiver(myReceiver);
+        super.onPause();
+    }
 
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.headphone, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.headphone, menu);
+        return true;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Headphone.this);
+        builder.setMessage("Is it functionality working properly?").setPositiveButton("Yes", dialogClickListener).setNegativeButton("No", dialogClickListener).show();
+    }
 }
